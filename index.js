63b829194bd007feead72f541a90fe7013123ed2
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-09-30 14:29:22 +0200

    Add POST request

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const port = process.env.PORT || 4000;
 const bodyParser = require('body-parser');
 
 const { getData } = require('./requests/get');
+const { postData } = require('./requests/post');
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => getData(req, res));
+app.post('/', (req, res) => postData(req, res));
 
 app.listen(port, () => console.log(`Proxy server listening on port ${port}`));
---
+++ requests/post.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+exports.postData = async (req, res) => {
+  try {
+    const { url } = req.query;
+    const { body, headers } = req;
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+
+    const response = await axios.post(url, body, { headers });
+    const { data: responseData } = await response;
+
+    return res.status(200).json(responseData);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
